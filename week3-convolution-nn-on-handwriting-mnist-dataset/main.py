625f0a5d4b5bb1d62e856ced84a5f8ac81695b25
Eduard <eduardtarassov@gmail.com>
2019-07-27 14:29:58 +0100

    Improved the accuracy

--- week3-convolution-nn-on-handwriting-mnist-dataset/main.py
+++ week3-convolution-nn-on-handwriting-mnist-dataset/main.py
@@ -32,8 +32,6 @@ x_test = x_test/255.0
 model = tf.keras.models.Sequential([
   tf.keras.layers.Conv2D(32, (3,3), activation='relu', input_shape=(28, 28, 1)),
   tf.keras.layers.MaxPooling2D(2, 2),
-  tf.keras.layers.Conv2D(32, (3,3), activation='relu'),
-  tf.keras.layers.MaxPooling2D(2,2),
   tf.keras.layers.Flatten(),
   tf.keras.layers.Dense(128, activation='relu'),
   tf.keras.layers.Dense(10, activation='softmax')
@@ -44,7 +42,7 @@ model.summary()
 
 # Too many epochs might cause overfitting - network learns the data from the training set really well,
 # but it's too specialised to only that data, and as a result is less effective at seeing other data
-model.fit(x_train, y_train, epochs=5)
+model.fit(x_train, y_train, epochs=10)
 print("-----------------------------------------------------")
 model.evaluate(x_test, y_test)
 
@@ -53,14 +51,14 @@ model.predict(x_test)
 
 
 import matplotlib.pyplot as plt
-f, axarr = plt.subplots(3,4)
+f, axarr = plt.subplots(3,2)
 FIRST_IMAGE=0
 SECOND_IMAGE=7
 THIRD_IMAGE=26
 CONVOLUTION_NUMBER = 2
 layer_outputs = [layer.output for layer in model.layers]
 activation_model = tf.keras.models.Model(inputs = model.input, outputs = layer_outputs)
-for x in range(0,4):
+for x in range(0,2):
   f1 = activation_model.predict(x_test[FIRST_IMAGE].reshape(1, 28, 28, 1))[x]
   axarr[0,x].imshow(f1[0, : , :, CONVOLUTION_NUMBER], cmap='inferno')
   axarr[0,x].grid(False)
