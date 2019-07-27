33c5974e6ad80b848c09ce8420a1fa544fefd191
Eduard <eduardtarassov@gmail.com>
2019-07-27 13:52:35 +0100

    Improved the accuracy and loss on MNIST handwritings dataset by adding convolution and pooling in the NN

--- week3-convolution-nn-on-handwriting-mnist-dataset/main.py
+++ week3-convolution-nn-on-handwriting-mnist-dataset/main.py
@@ -1,5 +1,10 @@
 import tensorflow as tf
-print(tf.__version__)
+
+
+# Original loss is 0.0277 and accuracy is 0.9916 on training data
+# Original loss is 0.0712 and accuracy is 0.9776 on test data
+# Convolutional NN loss is 0.019 and accuracy is 0.994 on training data
+# Convolutional NN loss is 0.0331 and accuracy is 0.9883 on test data
 
 
 # Need this in order to fix the CUDNN_STATUS_INTERNAL_ERROR
@@ -7,27 +12,21 @@ config = tf.ConfigProto()
 config.gpu_options.allow_growth = True
 session = tf.InteractiveSession(config=config)
 
+# Loading Handwriting mnist
+mnist = tf.keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+print(x_test)
+print("------------------------------------------")
 
 
-class myCallback(tf.keras.callbacks.Callback):
-  def on_epoch_end(self, epoch, logs={}):
-    if logs.get('acc') > 0.90:
-      print("\nReached 90% accuracy so cancelling training!")
-      self.model.stop_training = True
+
+x_train = x_train.reshape(60000, 28, 28, 1)
+x_train = x_train / 255.0
+x_test = x_test.reshape(10000, 28, 28, 1)
+x_test = x_test/255.0
 
 
-callbacks = myCallback()
-
-mnist = tf.keras.datasets.fashion_mnist
-(training_images, training_labels), (test_images, test_labels) = mnist.load_data()
-
-# Reshaping 60,000 28x28x1 items in a list into a 4D list that is 60,000x28x28x1.
-# This is because convolution requires single tensor.
-training_images=training_images.reshape(60000, 28, 28, 1)
-print(training_images)
-training_images=training_images / 255.0
-test_images = test_images.reshape(10000, 28, 28, 1)
-test_images=test_images/255.0
 
 #Added convolution + pooling + convolution + pooling = increased accuracy significantly.
 model = tf.keras.models.Sequential([
@@ -41,14 +40,16 @@ model = tf.keras.models.Sequential([
 ])
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 model.summary()
+
+
 # Too many epochs might cause overfitting - network learns the data from the training set really well,
 # but it's too specialised to only that data, and as a result is less effective at seeing other data
-model.fit(training_images, training_labels, epochs=5, callbacks=[callbacks])
+model.fit(x_train, y_train, epochs=5)
 print("-----------------------------------------------------")
-model.evaluate(test_images, test_labels)
+model.evaluate(x_test, y_test)
 
 print("=====================================================")
-model.predict(test_images)
+model.predict(x_test)
 
 
 import matplotlib.pyplot as plt
@@ -56,17 +57,17 @@ f, axarr = plt.subplots(3,4)
 FIRST_IMAGE=0
 SECOND_IMAGE=7
 THIRD_IMAGE=26
-CONVOLUTION_NUMBER = 1
+CONVOLUTION_NUMBER = 2
 layer_outputs = [layer.output for layer in model.layers]
 activation_model = tf.keras.models.Model(inputs = model.input, outputs = layer_outputs)
 for x in range(0,4):
-  f1 = activation_model.predict(test_images[FIRST_IMAGE].reshape(1, 28, 28, 1))[x]
+  f1 = activation_model.predict(x_test[FIRST_IMAGE].reshape(1, 28, 28, 1))[x]
   axarr[0,x].imshow(f1[0, : , :, CONVOLUTION_NUMBER], cmap='inferno')
   axarr[0,x].grid(False)
-  f2 = activation_model.predict(test_images[SECOND_IMAGE].reshape(1, 28, 28, 1))[x]
+  f2 = activation_model.predict(x_test[SECOND_IMAGE].reshape(1, 28, 28, 1))[x]
   axarr[1,x].imshow(f2[0, : , :, CONVOLUTION_NUMBER], cmap='inferno')
   axarr[1,x].grid(False)
-  f3 = activation_model.predict(test_images[THIRD_IMAGE].reshape(1, 28, 28, 1))[x]
+  f3 = activation_model.predict(x_test[THIRD_IMAGE].reshape(1, 28, 28, 1))[x]
   axarr[2,x].imshow(f3[0, : , :, CONVOLUTION_NUMBER], cmap='inferno')
   axarr[2,x].grid(False)
 plt.show()
